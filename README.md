849f6fdb96cec56be4ba4c1dceee978377e13250
bdorninger <bernhard.dorninger@scch.at>
2022-07-08 15:06:27 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-fh3fee
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-fh3fee)
---
+++ src/app/app.component.ts
@@ -0,0 +1,67 @@
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  VERSION,
+  ViewChild,
+} from '@angular/core';
+import * as d3d from 'd3-drag';
+import * as d3s from 'd3-selection';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit, AfterViewInit {
+  public name = 'Angular ' + VERSION.major;
+
+  @ViewChild('mydiv', {
+    read: ElementRef,
+    static: true,
+  })
+  public mydiv: ElementRef;
+
+  ngOnInit(): void {}
+
+  ngAfterViewInit() {
+    setTimeout(
+      () =>
+        (this.name = this.mydiv != null ? 'MYDIV injected' : 'Nothing there'),
+      100
+    );
+    if (this.mydiv) {
+      const ndiv = this.mydiv.nativeElement as HTMLDivElement;
+
+      // the "evil" event registration: causes globalZoneAwareCallback for 'touchmove' to become passive 
+      ndiv.addEventListener(
+        'touchmove',
+        (ev) => {
+          console.log('move! ', ev);
+          //ev.preventDefault();
+          //ev.stopImmediatePropagation();
+        },
+        {          
+          passive: true,
+        }
+      );
+
+      // although d3 tries to register its touchmove handler as active, the zone callback is already passive....
+      const dbh = d3d.drag(); //.container(ndiv);
+      dbh.on('start', (ev) => console.log('start', ev));
+      dbh.on('drag.touchable', (ev, arg) => this.dragged(ev, arg));
+      dbh.on('end', (ev) => console.log('end', ev));
+      d3s.select(ndiv).call(dbh);
+
+      
+    }
+  }
+
+  dragged(ev: any, arg: any) {
+    //d3-drag does prevent/stop anyway!
+    //ev.sourceEvent.preventDefault();
+    //ev.sourceEvent.stopImmediatePropagation();
+    console.log('drag', ev, arg);
+  }
+}
